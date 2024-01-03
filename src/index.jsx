b000bd4f60f50337a8dcc81c5c0bedb9f7aa14bb
LL <liaposhchenkol@gmail.com>
2024-01-03 20:59:57 +0100

    Cleanup, set up router

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,22 @@
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
+import { AdminPage } from './pages/AdminPage';
 import './global.css';
 
-createRoot(
-  document.querySelector('#app'),
-).render(<HomePage />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: 'admin',
+    element: <AdminPage />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+createRoot(document.querySelector('#app')).render(<App />);
