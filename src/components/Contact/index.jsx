32e8d50e67d2d0c62aef809eb7d46290d667736a
LL <liaposhchenkol@gmail.com>
2024-01-05 11:25:55 +0100

    Add state variables the form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,5 +1,3 @@
-// import './style.css';
-
 export const Contact = () => {
   return (
     <section className="dark">
--- src/components/RoomDetail/index.jsx
+++ src/components/RoomDetail/index.jsx
@@ -1,6 +1,33 @@
+import { useState } from 'react';
 import './style.css';
 
 export const RoomDetail = ({ type, price, src, description }) => {
+  const [dateFrom, setDateFrom] = useState('');
+  const [dateTo, setDateTo] = useState('');
+  const [numPeople, setNumPeople] = useState(1);
+  const [meal, setMeal] = useState('');
+  const [pet, setPet] = useState(false);
+  const [child, setChild] = useState(false);
+  const [accessible, setAccessible] = useState(false);
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  let mealPriceDay = 0;
+  if (meal === 'breakfast') {
+    mealPriceDay = 200;
+  } else if (meal === 'halfBoard') {
+    mealPriceDay = 300;
+  } else if (meal === 'fullBoard') {
+    mealPriceDay = 500;
+  }
+
+  const numDays = 3;
+  const roomPrice = numDays * numPeople * price;
+  const mealPrice = numDays * numPeople * mealPriceDay;
+  const petPrice = pet ? (roomPrice + mealPrice) * 0.25 : 0;
+  const childPrice = child ? (roomPrice + mealPrice) * 0.5 : 0;
+  const total = roomPrice + mealPrice + petPrice + childPrice;
+
   return (
     <section className="light">
       <div className="container">
@@ -17,52 +44,111 @@ export const RoomDetail = ({ type, price, src, description }) => {
               <label htmlFor="field1" className="field-label">
                 Od:
               </label>
-              <input id="field1" className="field-input" type="date" />
+              <input
+                onChange={(e) => setDateFrom(e.target.value)}
+                value={dateFrom}
+                id="field1"
+                className="field-input"
+                type="date"
+                required
+              />
               <label htmlFor="field2" className="field-label">
                 Do:
               </label>
-              <input id="field2" className="field-input" type="date" />
+              <input
+                onChange={(e) => setDateTo(e.target.value)}
+                value={dateTo}
+                id="field2"
+                className="field-input"
+                type="date"
+                required
+              />
 
               <label htmlFor="field2" className="field-label">
                 Počet osob:
               </label>
-              <input id="field3" className="field-input" type="text" />
+              <input
+                onChange={(e) => setNumPeople(e.target.value)}
+                value={numPeople}
+                id="field3"
+                className="field-input"
+                type="number"
+                required
+              />
               <label htmlFor="select" className="field-label">
                 Stravování:
               </label>
-              <select id="select" className="field-input">
-                <option>Vyberte</option>
-                <option>žádné</option>
-                <option>snídani</option>
-                <option>polopenzi</option>
-                <option>plnou penzi</option>
+              <select
+                onChange={(e) => setMeal(e.target.value)}
+                value={meal}
+                id="select"
+                className="field-input"
+                required
+              >
+                <option value="">Vyberte</option>
+                <option value="none">žádné</option>
+                <option value="breakfast">snídani</option>
+                <option value="halfBoard">polopenzi</option>
+                <option value="fullBoard">plnou penzi</option>
               </select>
               <label htmlFor="check1" className="field-label">
                 Domácí mazlíček:{' '}
               </label>
-              <input id="check1" className="field-input" type="checkbox" />
+              <input
+                onChange={(e) => setPet(!pet)}
+                value={pet}
+                id="check1"
+                className="field-input"
+                type="checkbox"
+              />
 
               <label htmlFor="check1" className="field-label">
                 Přistýlka pro dítě:{' '}
               </label>
-              <input id="check1" className="field-input" type="checkbox" />
+              <input
+                onChange={(e) => setChild(!child)}
+                value={child}
+                id="check1"
+                className="field-input"
+                type="checkbox"
+              />
 
               <label htmlFor="check1" className="field-label">
                 Bezbariérový přístup:{' '}
               </label>
-              <input id="check1" className="field-input" type="checkbox" />
+              <input
+                onChange={(e) => setAccessible(!accessible)}
+                value={accessible}
+                id="check1"
+                className="field-input"
+                type="checkbox"
+              />
 
               <label htmlFor="field2" className="field-label">
                 E-mail:{' '}
               </label>
-              <input id="field3" className="field-input" type="email" />
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                id="field3"
+                className="field-input"
+                type="email"
+                required
+              />
 
               <label htmlFor="field2" className="field-label">
                 Telefon:{' '}
               </label>
-              <input id="field3" className="field-input" type="tel" />
+              <input
+                onChange={(e) => setPhone(e.target.value)}
+                value={phone}
+                id="field3"
+                className="field-input"
+                type="tel"
+                required
+              />
             </div>
-            <h3>Celková cena za pobyt {} kč</h3>
+            <h3>Celková cena za pobyt {total} kč</h3>
             <button className="wide">Odeslat poptávku</button>
           </form>
         </div>
